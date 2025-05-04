ecd366fc9f50ed77d3fbad579d85aa4aeed7598b
lokesh <lokesh.s@datascopeplc.com>
2025-05-04 19:38:16 +0100

    Assets Inspection page

---
+++ cypress/pageObjects/addInspection.js
@@ -0,0 +1,63 @@
+class AddInspection{
+    URL="https://www.datascopesystem.com/Assets_Staging/Frontend/addInspection"
+    burgerMenu=".css-zvtom2 > .material-icons"
+    sideNavbar="#side-nav_button_add-inspection"
+    Asset=":nth-child(2) > .col-md-5 > .dx-field > .dx-field-value > .dx-show-invalid-badge > .dx-dropdowneditor-input-wrapper > .dx-texteditor-container > .dx-texteditor-input-container > .dx-texteditor-input"
+    inspectionDate="//input[@aria-haspopup='true']"
+    inspectorName="//input[@role='textbox']"
+    verdict="//*[@id='root']/div/div[3]/div/div/div/div/div[3]/div[2]/div/div[2]/div[1]/div"
+    file=".dx-fileuploader-input-wrapper > .dx-widget > .dx-button-content"
+    notes=".col-md-10 > .dx-field > .dx-field-value > .dx-show-invalid-badge > .dx-texteditor-container > .dx-texteditor-input-container > .dx-texteditor-input"
+    submit=".col-md-10 > .MuiButtonBase-root"
+    submitted = null;
+    close=".css-1b172wj > .MuiButtonBase-root"
+
+
+     openURL(){
+    cy.visit(this.URL)
+     }
+     clickburgerMenu(){
+        cy.get(this.burgerMenu).click()
+     }
+    
+    clicksideNavbar(){
+        cy.get(this.sideNavbar).click()
+    }
+
+    selectAsset(){
+        cy.get(this.Asset).click()
+        cy.contains('Loki Asset').click();
+        
+    }
+    chooseinspectionDate(){
+        cy.xpath(this.inspectionDate).invoke('val', '04/05/2025 06:18').trigger('change')
+    }
+    enterinspectorName(){
+        cy.xpath(this.inspectorName).type('Loki QA')
+    }
+    selectverdict(){
+        cy.xpath(this.verdict).click()
+        
+    cy.contains('Pass').should('be.visible').click();
+    }
+    selectfile() {
+        cy.get(this.file).selectFile('cypress/fixtures/Test 3.jpg', { action: 'drag-drop' });
+        
+
+    }
+    enternotes(){
+        cy.get(this.notes).type('Loki Add Inspection QA Automation Note')
+    }
+    clicksubmit(){
+        cy.get(this.submit).click()
+    }
+    assetsvalidateSubmitted(){
+        cy.xpath("//h2[contains(text(), 'INSPECTION SUBMITTED')]").should('be.visible').and('contain.text', 'INSPECTION SUBMITTED');
+
+}
+clickclose(){
+    cy.get(this.close).click()
+}
+}
+
+export default AddInspection
