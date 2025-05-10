63405dd3896e2d1bfb0eb9dcf199c952582bf163
lokesh <lokesh.s@datascopeplc.com>
2025-05-10 16:02:37 +0100

    lokesh

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'sucq2u',
   video: true,
   screenshotOnRunFailure: true,
   reporter: "cypress-mochawesome-reporter",
